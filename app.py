774676008003b0ca481f73f9ec9fd6b1b5f1d441
AyRawat <aayushrawat20@gmail.com>
2024-08-04 17:38:46 +0530

    added screenshot

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from nl2sql import invoke_agent
-import sqlite3
-
+import logging
 
 app = Flask(__name__)
 app.config["CORS_HEADERS"] = "Content-Type"
 CORS(app)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @app.route("/")
@@ -33,6 +34,12 @@ def query():
     try:
         response = invoke_agent(question, messages)
         print("the response", response)
+        logger.info(f"Question: {question}, Response: {response}")
         return jsonify({"response": response})
     except Exception as e:
+        logger.error(f"Error processing request: {str(e)}")
         return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(ssl_context=("cert.pem", "key.pem"))
--- prompts.py
+++ prompts.py
@@ -4,7 +4,6 @@ from langchain_core.prompts import (
     MessagesPlaceholder,
     FewShotPromptTemplate,
     SystemMessagePromptTemplate,
-    FewShotChatMessagePromptTemplate,
     PromptTemplate,
 )
 
@@ -23,30 +22,6 @@ If the question does not seem related to the database, just return "I don't know
 
 
 Here are some examples of user inputs and their corresponding SQL queries:"""
-
-custom_suffix = """Begin!
-Relevant pieces of previous conversation:
-{chat_history}
-(Note: Only reference this information if it is relevant to the current query.)
-
-
-Question: {input}
-Thought Process: It is imperative that I do not fabricate information not present in the database or engage in hallucination; 
-maintaining trustworthiness is crucial. Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
-You can order the results by a relevant column to return the most interesting examples in the database.
-Never query for all the columns from a specific table, only ask for the relevant columns given the question.
-You have access to tools for interacting with the database.
-Only use the given tools. Only use the information returned by the tools to construct your final answer.
-You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
-
-DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
-
-If the question does not seem related to the database, just return "I don't know" as the answer. 
-My final response must be delivered in the language of the user's query.
-
-{agent_scratchpad}
-"""
 
 
 few_shot_prompt = FewShotPromptTemplate(
